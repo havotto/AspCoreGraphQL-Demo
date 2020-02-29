5f2fc3dda78846b92e3399247f7de85f74d1433a
Otto Havasvolgyi <havasvolgyi.otto@gmail.com>
2020-03-01 00:55:27 +0100

    Query post by ID

--- GQL/GqlQueries/AppQuery.cs
+++ GQL/GqlQueries/AppQuery.cs
@@ -1,6 +1,9 @@
+using System.Linq;
 using AspCoreGraphQL.Entities.Context;
 using AspCoreGraphQL.GQL.GqlTypes;
+using GraphQL;
 using GraphQL.Types;
+using Microsoft.EntityFrameworkCore;
 
 namespace AspCoreGraphQL.GQL.GqlQueries
 {
@@ -9,6 +12,19 @@ namespace AspCoreGraphQL.GQL.GqlQueries
         public AppQuery(DataContext db)
         {
             Field<ListGraphType<PostType>>("posts", resolve: context => db.Posts);
+            Field<PostType>(
+                "post",
+                arguments: new QueryArguments(new QueryArgument<NonNullGraphType<IdGraphType>> { Name = "postId" }),
+                resolve: context =>
+                {
+                    int id;
+                    var argValue = context.GetArgument<string>("postId");
+                    if(!int.TryParse(argValue, out id)){
+                        context.Errors.Add(new ExecutionError($"Wrong value for int: {argValue}"));
+                    }
+                    return db.Posts.Where(p => p.Id == id).SingleOrDefaultAsync();
+                }
+            );
             Field<ListGraphType<CommentType>>("comments", resolve: context => db.Comments);
         }
     }
